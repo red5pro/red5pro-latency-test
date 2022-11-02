4e3ebf42cb066f84d917209fb5f9429a669fddb4
Mark Pace <pace@illuminati.org>
2022-11-02 16:30:46 -0700

    adjusting

--- clockfunctions.js
+++ clockfunctions.js
@@ -1,21 +1,4 @@
-function pad(num) {
-    if (num < 10) {
-        return "0" + num;
-    }
-    return "" + num;
-}
-function pad100(num) {
-    if (num < 100) {
-        return "0" + num;
-    }
-    return "" + num;
-}
-
 function clockDisplay() {
-    pad(ServerDate.getHours()) + ":" +
-    pad(ServerDate.getMinutes()) + ":" +
-    pad(ServerDate.getSeconds()) + "." +
-    pad100(ServerDate.getMilliseconds()); 
     var client = new Date();
     var offset = Math.abs(ServerDate - client);
     var precision = ServerDate.getPrecision();
