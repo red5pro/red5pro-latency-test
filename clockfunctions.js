865ba7981afe2c1266984eee7d13eb28c99bb72d
Mark Pace <pace@illuminati.org>
2022-11-02 16:59:56 -0700

    adjusting

--- clockfunctions.js
+++ clockfunctions.js
@@ -1,24 +1,3 @@
-function calcOffset() {
-    var xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-    xmlhttp.open("GET", "httpa://watchparty.red5.net/", false);
-    xmlhttp.send();
-
-    var dateStr = xmlhttp.getResponseHeader("Date");
-    var serverTimeMillisGMT = Date.parse(new Date(Date.parse(dateStr)).toUTCString());
-    var localMillisUTC = Date.parse(new Date().toUTCString());
-
-    offset = serverTimeMillisGMT -  localMillisUTC;
-    return offset;
-}
-
-function getServerTime() {
-    var date = new Date();
-
-    date.setTime(date.getTime() + offset);
-
-    return date;
-}
-
 function clockDisplay() {
     var client = new Date();
     var offset = Math.abs(ServerDate - client);
@@ -28,6 +7,5 @@ function clockDisplay() {
     var calcoffset = calcOffset();
     document.getElementById("server").innerHTML = server.toISOString();
     document.getElementById("offset").innerHTML = offset;
-    document.getElementById("calcOffset").innerHTML = calcoffset;
     document.getElementById("precision").innerHTML = precision;
 }
